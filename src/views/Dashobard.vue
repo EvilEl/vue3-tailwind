7baca6327d104995ff2e63115ec2adea8ef008ad
Mikhail <enc0ru@rambler.ru>
2022-05-15 18:31:47 +0300

    views test added

--- src/views/Dashobard.vue
+++ src/views/Dashobard.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>Dashobard</div>
+  <div class="dashboard"></div>
 </template>
 <script>
 export default {};
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.dashboard {
+  height: 100vh;
+  width: 100%;
+  background: url("@/assets/images/dashboards/bg.png") no-repeat center / cover;
+  background-position: 60% 25%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,56 @@
 <template>
-  <div>HOME</div>
+  <div class="home">
+    <div class="container">
+      <div class="home__content">
+        <h1 class="home__tite">Супер кресло</h1>
+        <p class="home__description">
+          Текст акции всегда отражает суть, а не просто болтовню, поэтому
+          внимательнее
+        </p>
+        <router-link class="home__link" to="Dashboard"> Подробнее </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
 </script>
-<style></style>
+<style lang="scss">
+.home {
+  height: 100vh;
+  width: 100%;
+  background: url("@/assets/images/dashboards/bg.png") no-repeat center / cover;
+  background-position: 60% 25%;
+  &__content {
+    padding-top: 192px;
+  }
+  &__tite {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 56px;
+    color: #ffffff;
+  }
+  &__description {
+    margin-top: 50px;
+    font-family: "Roboto";
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 143%;
+    color: #ffffff;
+  }
+  &__link {
+    display: inline-block;
+    margin-top: 50px;
+    background: #f13c6e;
+    border: 2px solid #ffffff;
+    border-radius: 40px;
+    padding: 13px 27px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 0.05em;
+  }
+}
+</style>
